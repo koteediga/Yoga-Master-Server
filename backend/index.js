65bfb0e1047e29efc2b16195c0d925083079b068
E.Koteswararao <105491279+koteediga@users.noreply.github.com>
2024-12-11 16:55:07 +0530

    2 commit

--- backend/index.js
+++ backend/index.js
@@ -8,10 +8,11 @@ console.log("Db username ",process.env.DB_USER)
 
 app.use(cors())
 app.use(express.json())
+
 //mando db connector
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@yogamaster.jzupr.mongodb.net/?retryWrites=true&w=majority&appName=yogamaster`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -38,18 +39,73 @@ async function run() {
     const appliedCollection=database.collection("applied");
 
 //classes routes here
-app.post('new-class',async(req,res)=>{
+app.post('/new-class',async(req,res)=>{
     const newClass=req.body;
     const result=await classesCollection.insertOne(newClass);
     res.send(result);
 })
+
+
+app.get('/classes',async(req,res)=>{
+  const query={status:'approved'};
+  const result =await classesCollection.find().toArray();
+  res.send(result);
+})
+
+//get classes  by insturctor email adress
+app.get('/classes/:email',async(req,res)=>{
+  const email=req.params.email;
+  const query={instructorEmail:email}
+  const result =await classesCollection.find(query).toArray();
+  res.send(result);
+})
+
+//manage classes
+app.get('/classes-manage',async(req,res)=>{
+  const result =await classesCollection.find().toArray();
+  res.send(result);
+})
+
+//update classes
+app.put('/change-status/:id',async(req,res)=>{
+  const id=req.params.id;
+  const status=req.body.status;
+  const reason=req.body.reason;
+  const filter={_id:new ObjectId(id)};
+  const options={upsert:true};
+  const updateDoc={
+    $set:{
+      status:status,
+      reason:reason,
+    },
+  }
+const result=await classesCollection.updateOne(filter,updateDoc,options)
+console.log("updated api called");
+res.send(result);
+})
+
+//appproved classe
+app.get('/approved-classes',async(req,res)=>{
+  const query={status:"approved"};
+  const result=await classesCollection.find(query).toArray();
+  res.send(result);
+})
+
+//get single classses
+ app.get('/class/:id',async(req,res)=>{
+  const id=req.params.id;
+  const query={_id:id}
+  const result=await classesCollection.findOne(query).toArray();
+  console.log("single id called")
+  res.send(result);
+ })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    //await client.close();
   }
 }
 run().catch(console.dir);
